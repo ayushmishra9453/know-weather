2cc6f68ca86a9ba5a44b2bc2267b99f25d1ff90d
ayushmishra9453 <ayushmishra9453@gmail.com>
2023-08-05 23:02:26 +0530

    a

--- tempapp/src/components/Tempapp.js
+++ tempapp/src/components/Tempapp.js
@@ -1,63 +1,64 @@
-import React,{useEffect, useState} from 'react';
-import "./css/style.css"
+import React, { useEffect, useState } from 'react';
+import './css/style.css';
 
-const Tempapp=()=> {
-  const [city,setCity]=useState(null);
-  const [search,setSearch]=useState("Mumbai");
+const Tempapp = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState('Mumbai');
 
-  useEffect(()=>{
-    const fetchApi= async ()=>{
-      const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=4d8787a7dd759641143714c9a45195cd`
-     const response=await fetch(url);
-    //  console.log(response)
-    const resJson=await response.json();
-    // console.log(resJson)
-    setCity(resJson.main)
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=4d8787a7dd759641143714c9a45195cd`
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Failed to fetch weather data');
+        }
+        const resJson = await response.json();
+        setCity(resJson.main);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setCity(null);
+      }
     };
     fetchApi();
-  },[search])
+  }, [search]);
+
   return (
     <>
-    <div className="box">
+      <div className="box">
         <div className="inputData">
-            <input
-            type='search'
+          <input
+            type="search"
             value={search}
-            className='inputField' 
-            onChange={(event)=>{
-            setSearch(event.target.value)
-            }}  />
+            className="inputField"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
         </div>
-        {!city ?(
-         <p className='errorMsg'>No data found</p>
-        ):(
+        {!city ? (
+          <p className="errorMsg">No data found</p>
+        ) : (
           <div>
-          <div className="info">
-            <h2 className='location'>
-            <i className="fas fa-street-view"></i>
-            </h2>
-            <h3>{search}</h3>
-            <h5 className="temp">
-            {city.temp}°Cel
-            </h5>
-           
-            <h5 className='tempmin_max' >Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel</h5>
-        </div>
-   
-    <div className="wave-one"></div>
-    <div className="wave-two"></div>
-    <div className="wave-three"></div>
-    </div>
-        )
-        }
+            <div className="info">
+              <h2 className="location">
+                <i className="fas fa-street-view"></i>
+              </h2>
+              <h3>{search}</h3>
+              <h5 className="temp">{city.temp}°Cel</h5>
+              <h5 className="tempmin_max">
+                Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel
+              </h5>
+            </div>
 
-       
-    </div>
+            <div className="wave-one"></div>
+            <div className="wave-two"></div>
+            <div className="wave-three"></div>
+          </div>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Tempapp
-
-
-// api.openweathermap.org/data/2.5/weather?q=pune&appid=4d8787a7dd759641143714c9a45195cd
+export default Tempapp;
