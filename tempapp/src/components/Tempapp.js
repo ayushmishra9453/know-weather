1e98615088fe4282a89766e5997d209a72f17dea
ayushmishra9453 <ayushmishra9453@gmail.com>
2023-08-05 22:57:49 +0530

    a

--- tempapp/src/components/Tempapp.js
+++ tempapp/src/components/Tempapp.js
@@ -34,13 +34,14 @@ const Tempapp=()=> {
           <div>
           <div className="info">
             <h2 className='location'>
-            <i className="fas fa-street-view"></i>{search}
+            <i className="fas fa-street-view"></i>
             </h2>
-            <h1 className="temp">
+            <h3>{search}</h3>
+            <h5 className="temp">
             {city.temp}°Cel
-            </h1>
+            </h5>
            
-            <h3 className='tempmin_max' >Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel</h3>
+            <h5 className='tempmin_max' >Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel</h5>
         </div>
    
     <div className="wave-one"></div>
